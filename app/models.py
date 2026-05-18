9b788497b353f57ab31f927bf4ea435b1dd7413a
Med Amine <med.amine10014@gmail.com>
2026-05-18 21:36:35 +0100

    created models

--- app/models.py
+++ app/models.py
@@ -1,14 +1,20 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
 import datetime
 
 
-class Monitor:
-    id: int
-    url: str
 
-class CheckResult:
-    id: int
-    monitor_id: int
+class Monitor(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    url: str
+    results: List["CheckResult"] = Relationship(back_populates="monitor")
+
+
+class CheckResult(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    monitor_id: int = Field(foreign_key="monitor.id", ondelete="CASCADE")
     status_code: int
     response_time: int
-    timestamp: datetime
+    timestamp: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
     is_up: bool
+    monitor: Optional[Monitor] = Relationship(back_populates="results")
